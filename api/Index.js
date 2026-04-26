f3706eddb405a6edf38a9ecb5d8442c31bbbfcfe
guysro <guykube100@gmail.com>
2026-04-26 15:23:35 +0300

    fixing

--- api/Index.js
+++ api/Index.js
@@ -22,7 +22,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
